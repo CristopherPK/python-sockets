f73c54c82e6d8b83cacd92a9e66b97df3a7ca71a
Cristopher Freitas <cristopherpk@gmail.com>
2015-07-17 09:11:41 -0300

    fixing data path.

--- bank/persistence.py
+++ bank/persistence.py
@@ -14,14 +14,14 @@ class Persistence(object):
         self._output = []
 
     def save(self, obj):
-        self._output = open(datapath + 'data.pkl', 'wa')
+        self._output = open('./data/data.pkl', 'wa')
         self._pickler = pkl.Pickler(self._output)
         self._pickler.dump(obj)
         self._output.close()
         print 'Dumping object'
 
     def load(self):
-        self._output = open(datapath + 'data.pkl', 'rb')
+        self._output = open('.data/data.pkl', 'rb')
         self._unpickler = pkl.Unpickler(self._output)
         return self._unpickler.load()
 
